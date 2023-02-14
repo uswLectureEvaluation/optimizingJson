dafb8bb3375dcbb9ae4d672e06cfa3fe681ce399
이진욱 <koownij@kakao.com>
2023-02-14 11:33:02 +0900

    Update EditJson.py

--- EditJson.py
+++ EditJson.py
@@ -1,7 +1,9 @@
 import json
 
+filename = "230214"
+
 # 수정할 json 파일 경로 입력
-with open('C:\\Users\\aaa\\Desktop\\TimetableData\\시간표데이터.json', 'r', encoding='utf-8') as f:
+with open('C:\\Users\\aaa\\Desktop\\TimetableData\\' + filename + '.json', 'r', encoding='utf-8') as f:
     json_data = json.load(f)
 
 # 삭제하면 안되는 키 배열
@@ -14,7 +16,7 @@ for data in json_data:
         data.pop(key)
 
 # 수정된 json 파일 저장 경로 입력
-with open('C:\\Users\\aaa\\Desktop\\TimetableData\\시간표데이터_수정완료.json', 'w', encoding='utf-8') as make_file:
+with open('C:\\Users\\aaa\\Desktop\\TimetableData\\' + filename + '_수정완료.json', 'w', encoding='utf-8') as make_file:
     json.dump(json_data, make_file, indent="\t", ensure_ascii=False)
     
 print(json.dumps(json_data,indent='\t' ,ensure_ascii=False))
